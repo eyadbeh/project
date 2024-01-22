4646141bd4155fe6c5d66c6e6e81bdb050ca9478
eyadbeh <152914987+eyadbeh@users.noreply.github.com>
2024-01-22 04:04:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 session_start();
 $host = "localhost";
 $username = "root";
-$password = "";
+$password = "123";
 $db = "ecommerce";
 
 $conn = mysqli_connect("$host", "$username", "$password", "$db");
